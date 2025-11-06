d3eaa0282ff24bb493175792b5ec1546023ec4d1
Affan-Rafique <affanrafique9@gmail.com>
2025-11-06 19:40:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
+// src/index.js
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";       // ✅ for React < 18
 import App from "./components/App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
